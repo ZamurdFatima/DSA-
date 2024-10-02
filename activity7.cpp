8347afbe0599f2715cef500d17c4e266742dc356
ZamurdFatima <fatimazamurd001@gmail.com>
2024-10-02 23:57:52 +0500

    7.activity

---
+++ activity7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int x;
+cin>>x;
+if (x > 0)
+ cout << "x is positive";
+else if (x < 0)
+ cout << "x is negative";
+else
+11
+ cout << "x is 0";
+}
